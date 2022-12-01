fdb4924ac5d3b32e78f623e44373e900d2de5b58
Sebistorm <seba7843@stud.kea.dk>
2022-12-01 10:46:40 +0100

    removed #main from CreatePollView

--- src/views/CreatePollView.vue
+++ src/views/CreatePollView.vue
@@ -1,39 +1,37 @@
 <template>
-    <div id="main">
-        <div id="content">
-            <div class="formContent">
-                <h1>Create Poll</h1>
-                <q-form @submit="onSubmit" class="form">
-                    <div class="left">
-                        <q-input class="mb-10" outlined v-model="title" label="Title *" lazy-rules
-                        :rules="[val => val && val.length > 0 || 'Please type something']" />
+    <div id="content">
+        <div class="formContent">
+            <h1>Create Poll</h1>
+            <q-form @submit="onSubmit" class="form">
+                <div class="left">
+                    <q-input class="mb-10" outlined v-model="title" label="Title *" lazy-rules
+                    :rules="[val => val && val.length > 0 || 'Please type something']" />
 
-                        <q-input class="mb-10" outlined type="textarea" v-model="description" label="Insert description here *" lazy-rules :rules="[
-                            val => val && val.length > 0 || 'Please type something'
-                        ]" />
+                    <q-input class="mb-10" outlined type="textarea" v-model="description" label="Insert description here *" lazy-rules :rules="[
+                        val => val && val.length > 0 || 'Please type something'
+                    ]" />
 
-                        <q-select class="mb-20" label="Write the answers here (Use Enter)" outlined v-model="answers" 
-                        use-input use-chips multiple hide-dropdown-icon input-debounce="0" @new-value="createValue"
-                        v-show="type!='CoWorker of the month'" />
-                    </div>
-                    <div class="right">
-                        <q-select class="mb-10" outlined v-model="type" :options="options" label="Select Type *"
-                        lazy-rules :rules="[val => val && val.length > 0|| 'Please select a type']" />
+                    <q-select class="mb-20" label="Write the answers here (Use Enter)" outlined v-model="answers" 
+                    use-input use-chips multiple hide-dropdown-icon input-debounce="0" @new-value="createValue"
+                    v-show="type!='CoWorker of the month'" />
+                </div>
+                <div class="right">
+                    <q-select class="mb-10" outlined v-model="type" :options="options" label="Select Type *"
+                    lazy-rules :rules="[val => val && val.length > 0|| 'Please select a type']" />
 
-                        <q-input class="mb-10" v-model="dateStart" outlined type="date" stack-label label="Start Date"
-                        lazy-rules :rules="[val => val && val.length > 0 || 'Please select a start date']" />
+                    <q-input class="mb-10" v-model="dateStart" outlined type="date" stack-label label="Start Date"
+                    lazy-rules :rules="[val => val && val.length > 0 || 'Please select a start date']" />
 
-                        <q-input class="mb-10" v-model="dateEnd" outlined type="date" stack-label label="End Date"
-                        lazy-rules :rules="[val => val && val.length > 0 || 'Please select a end date']" />
-                    </div>
-                    
-                    <div>
-                        <q-btn type="submit" style="background: var(--primary-red); color: var(--primary-white);" label="Create Poll" />
-                    </div>
-                </q-form>
-            </div>
-
+                    <q-input class="mb-10" v-model="dateEnd" outlined type="date" stack-label label="End Date"
+                    lazy-rules :rules="[val => val && val.length > 0 || 'Please select a end date']" />
+                </div>
+                
+                <div>
+                    <q-btn type="submit" style="background: var(--primary-red); color: var(--primary-white);" label="Create Poll" />
+                </div>
+            </q-form>
         </div>
+
     </div>
 </template>
 
